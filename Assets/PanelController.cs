5b344047ca4c8e625d098d1f6637f9b5d8ca10e8
alayham <alayham@hotmail.com>
2017-03-21 08:51:49 +0100

    created the panel controller

---
+++ Assets/PanelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour {
+
+    Slider slider;
+    Text text;
+    string caption = "";
+
+
+    public void changeText()
+    {
+        text.text = caption + " " + slider.value.ToString();
+    }
+
+    // Use this for initialization
+ 
+	void Start () {
+        slider = transform.Find("Slider").gameObject.GetComponent<Slider>();
+        text = transform.Find("Text").gameObject.GetComponent<Text>();
+        caption = text.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
